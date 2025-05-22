a41bbda611efd1a64fecf2baf5a7e8b3c7a11fa9
Srimanthy <yarrusrimanth1@gmail.com>
2025-05-22 15:40:38 -0500

    new

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-wdf4edws
+// server.js
+const http = require('http');
+http
+  .createServer((req, res) => res.end('Hello from myk8app!'))
+  .listen(3000, () => console.log('Listening on 3000'));
+
